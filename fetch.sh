69da83a31d222dc1f766531ec517f83cb3c485a6
a6.webm <46289515+a6-webm@users.noreply.github.com>
2024-02-16 16:15:38 +0000

    use excludes file instead of arguments

--- fetch.sh
+++ fetch.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rsync -a --delete -e ssh --exclude='.git' --exclude='check' --exclude='send.sh' --exclude='fetch.sh' blueManCluster:/user/home/sq21386/advanced-hpc-lbm/ /home/frerf/repos/advanced-hpc-lbm --info=ALL
+rsync -a --delete -e ssh --exclude-from='rsync-excludes.txt' blueManCluster:/user/home/sq21386/advanced-hpc-lbm/ /home/frerf/repos/advanced-hpc-lbm --info=ALL
--- send.sh
+++ send.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-rsync -a --delete -e ssh --exclude='.git' --exclude='check' --exclude='send.sh' --exclude='fetch.sh' /home/frerf/repos/advanced-hpc-lbm/ blueManCluster:/user/home/sq21386/advanced-hpc-lbm --info=ALL
+rsync -a --delete -e ssh --exclude-from='rsync-excludes.txt' /home/frerf/repos/advanced-hpc-lbm/ blueManCluster:/user/home/sq21386/advanced-hpc-lbm --info=ALL
